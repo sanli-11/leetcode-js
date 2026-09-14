c08efcc363b539271c0022aa5ed8147195108a1a
SanLi <sanli.has11@gmail.com>
2026-09-14 23:19:41 +0500

    74. Removed length=1 handler. Separate function isn't required. Flag is used to identify the row

--- problems/74-search-a-2d-matrix/solution.ts
+++ problems/74-search-a-2d-matrix/solution.ts
@@ -1,22 +1,5 @@
 function searchA2DMatrix(matrix: number[][], target: number) {
-  let binarySearch = (array: number[]) => {
-    if (array.length === 1) return array[0] === target;
-
-    let left = 0;
-    let right = array.length - 1;
-
-    while (left <= right) {
-      let mid = Math.floor((left + right) / 2);
-
-      if (array[mid] === target) return true;
-      else if (array[mid] > target) right = mid - 1;
-      else left = mid + 1;
-    }
-
-    return false;
-  };
-
-  if (matrix.length === 1) return binarySearch(matrix[0]);
+  let rowContainsRange: number[] = [];
 
   let firstRow = 0;
   let lastRow = matrix.length - 1;
@@ -24,9 +7,25 @@ function searchA2DMatrix(matrix: number[][], target: number) {
   while (firstRow <= lastRow) {
     let midRow = Math.floor((firstRow + lastRow) / 2);
 
-    if (matrix[midRow][0] <= target && matrix[midRow].at(-1)! >= target) return binarySearch(matrix[midRow]);
-    else if (matrix[midRow][0] > target) lastRow = midRow - 1;
-    else firstRow = midRow + 1;
+    if (matrix[midRow][0] > target) lastRow = midRow - 1;
+    else if (matrix[midRow].at(-1)! < target) firstRow = midRow + 1;
+    else {
+      rowContainsRange = matrix[midRow];
+      break;
+    }
+  }
+
+  if (firstRow > lastRow) return false;
+
+  let left = 0;
+  let right = rowContainsRange.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (rowContainsRange[mid] === target) return true;
+    else if (rowContainsRange[mid] > target) right = mid - 1;
+    else left = mid + 1;
   }
 
   return false;
