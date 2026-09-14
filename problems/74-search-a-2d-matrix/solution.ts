a29fd36f0707ec0497f45e46d75754132896a18a
SanLi <sanli.has11@gmail.com>
2026-09-14 22:32:46 +0500

    74 Passes O(log(m*n)) constraint

--- problems/74-search-a-2d-matrix/solution.ts
+++ problems/74-search-a-2d-matrix/solution.ts
@@ -1,9 +1,31 @@
 function searchA2DMatrix(matrix: number[][], target: number) {
-  for (let i = 0; i < matrix.length; i ++) {
-        if (matrix[i].indexOf(target) >= 0) return true;
-    }
+  let firstRow = 0;
+  let lastRow = matrix.length;
 
-    return false;
+  while (firstRow <= lastRow) {
+    let midRow = Math.floor((firstRow + lastRow) / 2);
+
+    if (
+      matrix[midRow][0] <= target &&
+      matrix[midRow][matrix.length - 1] >= target
+    ) {
+      let left = 0;
+      let right = matrix[midRow].length;
+
+      while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (matrix[midRow][mid] === target) return true;
+        else if (matrix[midRow][mid] > target) right = mid - 1;
+        else left = mid + 1;
+      }
+
+      return false;
+    } else if (matrix[midRow][0] > target) lastRow = midRow - 1;
+    else firstRow = midRow + 1;
+  }
+
+  return false;
 }
 
 export { searchA2DMatrix };
