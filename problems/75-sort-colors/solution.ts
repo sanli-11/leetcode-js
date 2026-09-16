d2e7f047cf172abe2c416904a1572296311e6e3f
SanLi <sanli.has11@gmail.com>
2026-09-17 00:10:47 +0500

    75. Selection sort is slower in best case scenario

--- problems/75-sort-colors/solution.ts
+++ problems/75-sort-colors/solution.ts
@@ -1,10 +1,10 @@
 function sortColors(nums: number[]): number[] {
   for (let i = 0; i < nums.length; i++) {
-    for (let j = 0; j < nums.length - i - 1; j++) {
-      if (nums[j] > nums[j + 1]) {
-        let temp = nums[j];
-        nums[j] = nums[j + 1];
-        nums[j + 1] = temp;
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] > nums[j]) {
+        let temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
       }
     }
   }
