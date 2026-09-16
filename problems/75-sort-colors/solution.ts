b9fa503c2ba40a5e68dd109a83521231e1672203
SanLi <sanli.has11@gmail.com>
2026-09-17 00:09:52 +0500

    75. Bubble sort passes memory constraint of not creating any new arrays

--- problems/75-sort-colors/solution.ts
+++ problems/75-sort-colors/solution.ts
@@ -1,45 +1,13 @@
 function sortColors(nums: number[]): number[] {
-  function merge(left: number[], right: number[]): number[] {
-    const res = [];
-    let l = 0, r = 0;
-
-    while (l < left.length && r < right.length) {
-      if (left[l] < right[r]) {
-        res.push(left[l]);
-        l++;
-      } else {
-        res.push(right[r]);
-        r++;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length - i - 1; j++) {
+      if (nums[j] > nums[j + 1]) {
+        let temp = nums[j];
+        nums[j] = nums[j + 1];
+        nums[j + 1] = temp;
       }
     }
-
-
-    while (l < left.length) {
-      res.push(left[l])
-      l ++
-    }
-    while (r < right.length) {
-      res.push(right[r])
-      r ++
-    }
-
-    return res;
   }
-
-  function mergeSort(array: number[]): number[] {
-    if (array.length <= 1) return array;
-
-    const mid = Math.floor(array.length / 2);
-
-    const left: number[] = mergeSort(array.slice(0, mid));
-    const right: number[] = mergeSort(array.slice(mid));
-
-    return merge(left, right);
-  }
-
-  mergeSort(nums).forEach((n) => nums.push(n));
-
-  nums.splice(0, Math.floor(nums.length / 2))
 
   return nums;
 }
