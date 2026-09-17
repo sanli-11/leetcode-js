f70dc8a678a7e6a8cee895ec44710e22cabf994f
SanLi <sanli.has11@gmail.com>
2026-09-17 23:26:09 +0500

    73. Swapped Array.forEach with for loop

--- problems/73.set-matrix-zeroes/solution.ts
+++ problems/73.set-matrix-zeroes/solution.ts
@@ -2,18 +2,20 @@ function setZeroes(matrix: number[][]): number[][] {
   const zeroX: Set<number> = new Set();
   const zeroY: Set<number> = new Set();
 
-  matrix.forEach((row, i) =>
-    row.forEach((num, j) => {
-      if (num === 0) {
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === 0) {
         zeroX.add(i);
         zeroY.add(j);
       }
-    }),
-  );
+    }
+  }
 
   zeroX.forEach((x: number) => (matrix[x] = Array(matrix[0].length).fill(0)));
 
-  zeroY.forEach((y) => matrix.forEach((m) => (m[y] = 0)));
+  zeroY.forEach((y) => {
+    for (let k = 0; k < matrix.length; k++) matrix[k][y] = 0
+  });
 
   return matrix;
 }
