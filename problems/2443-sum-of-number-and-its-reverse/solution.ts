8457dba3f1d7d22bb9924d5388e591909b332660
SanLi <sanli.has11@gmail.com>
2026-09-19 13:53:42 +0500

    2443. Using modulus & divide to find reverse

--- problems/2443-sum-of-number-and-its-reverse/solution.ts
+++ problems/2443-sum-of-number-and-its-reverse/solution.ts
@@ -1,25 +1,16 @@
 function sumOfNumberAndItsReverse(num: number): boolean {
   if (num === 0) return true;
 
-  for (let i = 2; i < num; i++) {
-    if (num % 2 !== 0 && num % 11 !== 0 && num % i === 0) return false;
-  }
-
-  let reversed: number[] = [0];
-
   for (let i = 1; i < num; i++) {
-    reversed[0] += 1;
+    let rev = 0,
+      n = i;
 
-    for (let j = 0; j < reversed.length; j++) {
-      if (reversed[j] > 9) {
-        reversed[j] = 0;
-
-        if (j === reversed.length - 1) reversed.push(1);
-        else reversed[j + 1] += 1;
-      }
+    while (n > 0) {
+      rev = rev * 10 + (n % 10);
+      n = Math.floor(n / 10);
     }
 
-    if (i + Number(reversed.join("")) === num) return true;
+    if (i + rev === num) return true;
   }
 
   return false;
