87f126b3ff61ddc6ef1ae1443cd4897e3df9b624
SanLi <sanli.has11@gmail.com>
2026-09-21 22:09:54 +0500

    71. go: Using strings library

--- problems/71-simplify-path/solution.go
+++ problems/71-simplify-path/solution.go
@@ -1,46 +1,24 @@
 package main
 
+import "strings"
+
 func simplifyPath(path string) string {
-	if len(path) <= 1 {
-		return path
-	}
-
-	var sections []string = []string{}
-	var start int = 0
-
-	for i, iV := range path {
-		if iV == '/' {
-			sections = append(sections, path[start:i])
-			start = i + 1
-		}
-	}
-	sections = append(sections, path[start:])
-
+	var sections []string = strings.Split(path, "/")
 	var res []string = []string{}
 
 	for _, iV := range sections {
-		if len(iV) >= 1 {
-			switch iV {
-			case ".":
-			case "..":
-				if len(res) >= 1 {
-					res = res[:len(res)-1]
-				}
-			default:
-				res = append(res, iV)
+		if len(iV) < 1 || iV == "." {
+			continue
+		}
+
+		if iV == ".." {
+			if len(res) >= 1 {
+				res = res[:len(res)-1]
 			}
+		} else {
+			res = append(res, iV)
 		}
 	}
 
-	if len(res) < 1 {
-		return "/"
-	}
-
-	var result string = ""
-
-	for _, iV := range res {
-		result += "/" + iV
-	}
-
-	return result
+	return "/" + strings.Join(res, "/")
 }
