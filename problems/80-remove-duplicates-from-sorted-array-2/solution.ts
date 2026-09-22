a7bff5631ae8106d8d98fbee65d2cf116df7877a
SanLi <sanli.has11@gmail.com>
2026-09-22 22:02:06 +0500

    80. Using two pointers

--- problems/80-remove-duplicates-from-sorted-array-2/solution.ts
+++ problems/80-remove-duplicates-from-sorted-array-2/solution.ts
@@ -1,27 +1,16 @@
 function removeDuplicates(nums: number[]): number {
-  let firstItem = 0;
-  let currentItem = nums[0];
-  let countItem = 1;
+  let left = 2;
 
-  for (let i = 1; i < nums.length; i++) {
-    if (currentItem === nums[i]) countItem++;
-    else {
-      if (countItem > 2) {
-        nums.splice(firstItem + 2, countItem - 2);
-        i -= countItem - 1;
-      }
-
-      currentItem = nums[i];
-      firstItem = i;
-      countItem = 1;
+  for (let right = 2; right < nums.length; right++) {
+    if (nums[left - 2] !== nums[right]) {
+      nums[left] = nums[right]
+      left++;
     }
   }
 
-  if (countItem > 2) nums.splice(firstItem + 2, countItem - 2);
-
   console.log(nums);
 
-  return nums.length;
+  return left;
 }
 
 export { removeDuplicates };
